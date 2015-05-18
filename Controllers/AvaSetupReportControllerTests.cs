5592107911552bdc04d1f4da272e614631d4d8cd
Kishor-Dudhe <kishor_dudhe@yahoo.com>
2015-05-18 20:20:26 +0530

    Unit Test on TPA using Nunit

---
+++ Controllers/AvaSetupReportControllerTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxProfileAssistant.Controllers;
+using NUnit.Framework;
+using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Serialization;
+using TaxProfileAssistant.Models;
+using TaxProfileAssistant.Utils;
+using System.Data.Entity;
+
+namespace TaxProfileAssistantTest.Controllers.Tests
+{
+    
+    [TestFixture()]
+    public class AvaSetupReportControllerTests
+    {
+        AccountInfo accountInfo;
+        AvaSetupReportController avaSetupReportController;
+        AccountService accountService;
+        TPAScaffold input;
+        TaxProfileAssistantController tpaController;
+
+        string key;
+        
+        [SetUp]//Initializes data required for each test and runs once per test method.
+        public void InitializeData()
+        {
+            //Arrange
+
+            avaSetupReportController = new AvaSetupReportController();
+            XmlReader xmlReader = XmlReader.Create(@"..\XMLTestFiles\TPA_500.xml");
+            var tpa = new TPAScaffold();
+            var serializer = new XmlSerializer(tpa.GetType());
+            input = (TPAScaffold)serializer.Deserialize(xmlReader);
+
+            tpaController = new TaxProfileAssistantController();
+            tpaController.ControllerContext = new ControllerContext();
+            string resultstr = tpaController.Post(input);
+            key = resultstr.Substring(38);
+            key = key.Replace(Constants.APPLICATION_URL_DUMMY_TEXT, "/");
+            accountInfo = AvaTaxProfileAssistantHelper.GetAccountInfoBySecureKey(key);
+                     
+            accountService = new AccountService(accountInfo.Webservice, accountInfo.UserName, accountInfo.Password, accountInfo.AccountNumber, accountInfo.LicenseKey, accountInfo.ERPName);
+
+
+            TempDataDictionary TempData = new TempDataDictionary();
+            avaSetupReportController.TempData.Add("AccountInfo", accountInfo);
+            avaSetupReportController.TempData.Add("AccountService", accountService);
+            Constants.SESSION_KEY_SELECTED_COMPANY = "4.1 Dev Company";
+            avaSetupReportController.TempData.Add(Constants.SESSION_KEY_SELECTED_COMPANY, "4.1 Dev Company");
+            avaSetupReportController.TempData.Add(TaxProfileAssistant.Utils.Constants.SESSION_KEY_ERP_NAME, accountInfo.ERPName);
+            avaSetupReportController.TempData.Add(TaxProfileAssistant.Utils.Constants.SESSION_KEY_COMPANY_NAME, "Systems of America");
+
+        }
+
+        [Test()]//Checks if An object of FinishSetup model is returned through view of index method.
+        public void AvaSetupReportController_IndexMethod_ReturnsFinishSetupObjectTest()
+        {
+            //Act
+            var result = avaSetupReportController.Index(key) as ViewResult;
+
+            //Assert
+            //Assert.IsInstanceOfType(result.Model, typeof(FinishSetup), "Incorrect type of result returned!!Expected is an object of FinishSetup!");
+            //Assert.IsInstanceOf(typeof(FinishSetup), result.Model, "Incorrect type of result returned!!Expected is an object of FinishSetup!");
+            Assert.IsInstanceOf<FinishSetup>(result.Model, "Incorrect type of result returned!!Expected is an object of FinishSetup!");         
+        } 
+
+
+                
+        [Test()]//Checks if Index method sets the appropriate field to true
+        public void AvaSetupReportController_IndexMethod_SetsTheRequiredFieldsToTrue()
+        {
+                      
+            //Act
+
+            var result = avaSetupReportController.Index(key) as ViewResult ;
+            var resultData=(FinishSetup)result.ViewData.Model;
+
+            //Assert
+            
+            Assert.IsTrue(resultData.IsConnected, "Service Connection failed!!");
+            Assert.IsTrue(resultData.IsTaxScheduledMapped, "Tax Schedule is not mapped to AVATAX!!");
+            Assert.IsNotNull(resultData.NexusList, "Nexus List Cannot be empty!!");
+            Assert.IsNotNull(resultData.HelpLinkList, "HelpLinks should be provided!!No Help Links generated!");
+            Assert.IsNotNull(resultData.TotalCustomers, "There are no customers for the respective ERP!!");
+            Assert.IsTrue(resultData.IsAddressValidationEnabled, "Address Validation not enabled!!");
+
+
+        }
+    }
+}
---
+++ Controllers/CreateCompanyControllerTests.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxProfileAssistant.Controllers;
+using NUnit.Framework;
+using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Serialization;
+using TaxProfileAssistant.Models;
+using TaxProfileAssistant.Utils;
+using System.Data.Entity;
+
+
+namespace TaxProfileAssistantTest.Controllers.Tests
+{
+    [TestFixture()]
+    public class CreateCompanyControllerTests
+    {
+        AccountInfo accountInfo;
+        AccountService accountService;
+        CreateCompanyController createCompanyController;
+        TempDataDictionary TempData;
+        TPAScaffold input;
+        TaxProfileAssistantController tpaController;
+        string key;
+        FormCollection fc;
+        
+        [SetUp]//Initializes data required for each test and runs once per test method.
+        public void InitializeData()
+        {
+            accountInfo = new AccountInfo();
+            XmlReader xmlReader = XmlReader.Create(@"..\XMLTestFiles\TPA_500.xml");
+            var tpa = new TPAScaffold();
+            var serializer = new XmlSerializer(tpa.GetType());
+            input = (TPAScaffold)serializer.Deserialize(xmlReader);
+
+            tpaController = new TaxProfileAssistantController();
+            tpaController.ControllerContext = new ControllerContext();
+            string resultstr = tpaController.Post(input);
+            key = resultstr.Substring(38);
+            key = key.Replace(Constants.APPLICATION_URL_DUMMY_TEXT, "/");
+            accountInfo = AvaTaxProfileAssistantHelper.GetAccountInfoBySecureKey(key);
+            createCompanyController = new CreateCompanyController();
+            accountService = new AccountService(accountInfo.Webservice, accountInfo.UserName, accountInfo.Password, accountInfo.AccountNumber, accountInfo.LicenseKey, accountInfo.ERPName);
+            TempData = new TempDataDictionary();
+            createCompanyController.TempData.Add("AccountInfo", accountInfo);
+            createCompanyController.TempData.Add("AccountService", accountService);
+
+            fc = new FormCollection();
+            fc["applicationUrlKey"] = key;
+
+
+
+        }
+   
+        [Test()]//checks if index method returns company object through view
+        public void CreateCompanyController_IndexTest_CheckIfItReturnsCompanyObject()
+        {
+            //Arrange 
+
+            fc = new FormCollection();
+            fc["applicationUrlKey"] = key;
+
+            //Act
+            
+            var result = createCompanyController.Index(key,fc) as ViewResult;
+
+            //Assert
+
+            //Assert.IsInstanceOfType((result).Model, typeof(CreateCompany), "Incorrect Type of result returned,Return type should be an object of createCompany");           
+            Assert.IsInstanceOf<CreateCompany>(result.Model,"Incorrect Type of result returned,Return type should be an object of createCompany");
+                 
+        }
+
+        [Test()]//Checks if the method redirects to nexusSetup controller if company is created.
+        public void CreateComapnyController_CreateNewCompanyTestToCheckIfItRedirectsToNexusSetupOnceCompanyIsCreated()
+        {
+           
+
+            CreateCompany createCompany = new CreateCompany();
+            createCompany.AccountInfoID = accountInfo.ID;
+            createCompany.AddressLine1 = "900 winslow way e";
+            createCompany.AddressLine2 = "";
+            createCompany.AddressLine3 = "";
+            createCompany.CompanyCode = Guid.NewGuid().ToString().Substring(0, 8);
+            createCompany.CompanyName = "Systems of America1";
+            createCompany.Country = "US";
+            createCompany.StateProvince = "WA";
+            createCompany.TIN = "95-3657472";
+           
+            createCompany.FirstName = "abbott";
+            createCompany.LastName = "abbott";
+            createCompany.PhoneNumber = "9999999999";
+            createCompany.Fax = "9999999999";
+            createCompany.Email = "abc@abc.com";
+            createCompany.City = "Bainbridge Island";
+            createCompany.ZipCode = "98110";
+            createCompany.MobileNumber = "";
+            createCompany.Title = "";
+            createCompany.BIN = "";
+
+            fc = new FormCollection();
+            fc["applicationUrlKey"] = key;
+
+            //Act
+            var companyresult = createCompanyController.CreateNewCompany(createCompany,fc);
+           
+
+            //Assert
+
+            RedirectToRouteResult routeResult = companyresult as RedirectToRouteResult;
+            Assert.AreEqual(routeResult.RouteValues["action"], "Index", "Index Method of CreateCompany Controller failed to redirect to correct action!!");
+            Assert.AreEqual(routeResult.RouteValues["controller"], "NexusSetup", "Index Method of CreateCompany Controller failed to redirect to correct controller!!");
+           
+        }
+
+        [Test()]//Checks if the method returns correct view if company is not created
+        public void CreateComapnyController_CreateNewCompanyTestToCheckIfItReturnsCreateCompanyIndexViewWhenCompanyIsNotCreated()
+        {
+          
+
+            CreateCompany createCompany = new CreateCompany();
+            createCompany.AccountInfoID = accountInfo.ID;
+            createCompany.AddressLine1 = "900 winslow way e";
+            createCompany.AddressLine2 = "";
+            createCompany.AddressLine3 = "";
+
+            fc = new FormCollection();
+            fc["applicationUrlKey"] = key;
+            
+            
+            //Act
+
+            var companyresult = createCompanyController.CreateNewCompany(createCompany,fc) as ViewResult ;
+
+
+            //Assert
+            Assert.AreEqual("Index", companyresult.ViewName);
+             
+
+        }
+    }
+}
